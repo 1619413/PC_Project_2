109e39b9854762a4acc606e8285f652dcb091168
1619413 <1619413@students.wits.ac.za>
2021-11-11 09:39:28 +0200

    progress on MPI

--- pMainMpi.cpp
+++ pMainMpi.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<limits.h>
 #include<sstream>
-#include<vector>
+#include<vector
+#include<algorithm>
+#include<mpi.h>
+
 
 using namespace std;
 
@@ -16,11 +19,17 @@ class Node{
     }
 };
 
-int main(){
-    int numVertices=4;
+int main(int argc,char* argv[]){
+    auto Compare = [](Node a, Node b) { return a.dist <b.dist ;};
+
+    int numVertices;
     string line,tmp;
 
-    vector<vector<int>>adjMatrix(numVertices);
+    vector<int>dist(numVertices,INT_MAX);//local
+    dist[sourceNode]=0;
+    vector<Node>myMinVec;//local
+    vector<vector<int>>adjMatrix(numVertices);//local
+    vector<bool>explored(numVertices,false);//local
     //Initialize the adj matrix
     for(int i=0;i<numVertices;i++){
         getline(cin,line);
@@ -41,6 +50,53 @@ int main(){
         }
         cout<<endl;
     }
+
+    //MPI PARALLEL APPROACH
+    MPI_Init(&argc,&argv);
+    int mpiSize,mpiRank;
+    int mpiRoot=0;
+    MPI_Comm_size(MPI_COMM_WORLD,&mpiSize);
+    MPI_Comm_rank(MPI_COMM_WORLD,&mpiRank);
+
+    int globalMinimumDistance;
+    int globalMinimumVertex;
+
+    if(mpiRank==mpiRoot){
+        globalMinimumDistance=INT_MAX;
+        globalMinimumVertex=0;
+        numVertices=4;
+    }
+
+    //Broadcast the main int variables
+    MPI_Bcast(&globalMinimumDistance,1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
+    MPI_Bcast(&globalMinimumVertex,1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
+    MPI_Bcast(&numVertices,1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
+    //Broadcast vectors
+    MPI_Bcast(&dist,numVertices,MPI_INT,mpiRoot,MPI_COMM_WORLD);
+    MPI_Bcast(&explored,numVertices,MPI_INT,mpiRoot,MPI_COMM_WORLD);
+    for(int i=0;i<numVertices;i++){
+        MPI_Bcast(&adjMatrix[i],numVertices,MPI_INT,mpiRoot,MPI_COMM_WORLD);
+    }
+
+
+    int myFirstVertexIndex=(mpiRank*numVertices)/mpiSize;
+    int myLastVertexIndex=((mpiRank+1)*numVertices)/mpiSize-1;
+
+    //Copy locally assigned vertices to local vector
+    for(int i=myFirstVertexIndex;i<=myLastVertexIndex;i++){
+        myMinVec.push_back(Node(i,dist[i]));
+    }
+
+    //sort the local vector
+    sort(myMinVec.begin(),myMinVec.end(),Compare);
+
+    while(!explored[destination]){
+        for(int k=0;k<myMinVec.size();k++){
+            Node v=myMinVec[k];
+        }
+    }
+
+    MPI_Finalize();
     return 0;
 
 
