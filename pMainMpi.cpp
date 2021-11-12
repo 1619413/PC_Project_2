a15c999751f7b957c980ab02109716c1ae66dc1b
1619413 <1619413@students.wits.ac.za>
2021-11-12 16:57:34 +0200

    removed comments, added graphGen and timings

--- pMainMpi.cpp
+++ pMainMpi.cpp
@@ -23,6 +23,170 @@ class Node{
     }
 };
 
+void genRandomGraph(int numNodes){
+    int row=0;
+    int col=0;
+    int maxEdges=numNodes*(numNodes-1),currEdges=0;
+    float randomNum,p=0.00001, currDensity=0, goalDensity=0.35;
+    // bool exitLoop=false;
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine e(seed);
+
+    //Setup adjacency matrix
+    vector<vector<int>>adjMatrix(numNodes,vector<int>(numNodes,0));
+    //Maximum number of edges in our graph n(n-1)/2 -----> use this for density calculation
+    for(row=0;row<numNodes;row++){
+        // if(exitLoop){
+        //     break;
+        // }
+        for(col=0;col<numNodes;col++){
+            randomNum=(float)rand()/RAND_MAX;
+            if(randomNum<=p){
+                currDensity=(float)currEdges/maxEdges;
+                // cout<<currDensity<<endl;
+                if(row!=col){//
+                    if(currDensity<goalDensity){//Only go through one side of the main diagonal
+                        adjMatrix[row][col]=1;
+                        adjMatrix[col][row]=1;
+                        currEdges+=2;
+                    }
+                    else{
+                        row=numNodes;
+                        break;
+                    }
+
+
+                }
+
+            }
+
+        }
+        row+=1;
+        col+=1;
+    }
+    //Random walk
+    vector<int>randomWalk;
+    vector<bool>vistitedRandomWalk(numNodes,false);
+    int currentNodeIndex=0;
+    for(int randomWalkIndex=0; randomWalkIndex<numNodes;randomWalkIndex++){
+        randomWalk.push_back(randomWalkIndex);
+    }
+
+    shuffle(randomWalk.begin(),randomWalk.end(),e);
+
+    for(int randomWalkIndex=0;randomWalkIndex<randomWalk.size();randomWalkIndex++){
+        if(randomWalk[randomWalkIndex]!=0){
+            adjMatrix[currentNodeIndex][randomWalk[randomWalkIndex]]=1;
+            adjMatrix[randomWalk[randomWalkIndex]][currentNodeIndex]=1;
+            currentNodeIndex=randomWalk[randomWalkIndex];
+        }
+    }
+
+    // cout<<endl;
+    // cout<<"RANDOM GENERATED GRAPH: "<<endl;
+    // for(int row=0;row<numNodes;row++){
+    //     for(int col=0;col<numNodes;col++){
+    //         cout<<adjMatrix[row][col]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    ofstream myWriteFile;
+    myWriteFile.open("input.txt",ofstream::trunc);
+    if (myWriteFile.is_open()) {
+        myWriteFile<<numNodes<<"\n";
+
+        string line="";
+
+        for(int outRow=0;outRow<numNodes;outRow++){
+            for(int outCol=0;outCol<numNodes;outCol++){
+                line+=to_string(adjMatrix[outRow][outCol]);
+            }
+            myWriteFile<<line<<"\n";
+            line="";
+        }
+    }
+}
+
+int serialDijkstra(vector<vector<int>>adjacencyMatrix, int numNodes, int sourceVert, int destVert){
+    auto Compare = [](Node a, Node b) { return a.dist <b.dist ;};
+
+    vector<Node>minVec;
+    // set all vertices to unexplored
+    vector<bool>explored(numNodes,false);
+    int dist[numNodes];
+    // int prev[numNodes];
+    // For each vertex v:
+    for(int v=0;v<numNodes;v++){
+        dist[v]=INT_MAX;
+        // prev[v]=-1;
+    }
+    dist[sourceVert]=0;
+    //Add all the nodes to the priority queue
+    for(int i=0;i<numNodes;i++){
+        minVec.push_back(Node(i,dist[i]));
+    }
+
+    sort(minVec.begin(),minVec.end(),Compare);
+
+    // while desination is not explored:
+    while(!explored[destVert]){
+        // v = least-value unexplored vertex
+        for(int k=0;k<numNodes;k++){
+            Node v=minVec[k];
+            //make sure it is unexplored aswell
+            if(explored[v.id]==false){
+                minVec.erase(minVec.begin());
+                // set v to explored
+                int vId=v.id;
+                explored[vId]=true;
+                // for each edge (v,w):
+                for(int i=0;i<numNodes;i++){
+                    if(adjacencyMatrix[vId][i]==1){
+                    // if dist[v]+len(v,w)<dist[w]:
+                        if(dist[vId]+1<dist[i]){
+                            // dist[w]=dist[v]+len(v,w)
+                            dist[i]=dist[vId]+1;
+                            //Find the updated node in the vector
+                            for(int j=0;j<numNodes;j++){
+                                if(minVec[j].id==i){
+                                    minVec[j].dist=dist[vId]+1;
+                                }
+                            }
+                            // Update prev
+                            // prev[i]=vId;
+                            //Sort the vector
+                            sort(minVec.begin(),minVec.end(),Compare);
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+
+
+    }
+    // cout<<"The path to the destination to the source node is: ";
+    // int currNode=destVert;
+    // while(currNode!=-1){
+    //     cout<<currNode<<" ";
+    //     currNode=prev[currNode];
+    // }
+    // cout<<endl;
+
+    return dist[destVert];
+}
+
+void verify(int i1,int i2){
+    cout<<endl;
+    if(i1==i2){
+        cout<<"SERIAL AND PARALLEL IMPLEMENTATION OUTPUTS ARE THE SAME"<<endl;
+    }else{
+        cout<<"SERIAL AND PARALLEL IMPLEMENTATION OUTPUTS ARE NOT THE SAME"<<endl;
+    }
+}
+
 int main(int argc,char* argv[]){
     //MPI PARALLEL APPROACH
     MPI_Init(&argc,&argv);
@@ -31,6 +195,7 @@ int main(int argc,char* argv[]){
     MPI_Comm_size(MPI_COMM_WORLD,&mpiSize);
     MPI_Comm_rank(MPI_COMM_WORLD,&mpiRank);
 
+    double parallelStarttime,parallelEndtime;
     vector<vector<int>>adjMatrix;
     vector<int>dist;
     int numVertices;
@@ -55,15 +220,14 @@ int main(int argc,char* argv[]){
                 stringstream s(line);
                 s>>numNodes;
                 adjMatrix.resize(numNodes,vector<int>(numNodes,0));
-                // cout<<line<<endl;
-                // cout<<numNodes<<endl;
+
                 for(int rowsInput=0;rowsInput<numNodes;rowsInput++){
                     getline(myReadFile,line);
                     stringstream s(line);
 
                     s>>tmp;
                     for(int colsInput=0;colsInput<numNodes;colsInput++){
-                        adjMatrix[rowsInput][colsInput]=(int)(tmp[colsInput])-48;//TODO: NEED TO FIX WHEN WEIGHTS CONSIST OF 2 DIGITS
+                        adjMatrix[rowsInput][colsInput]=(int)(tmp[colsInput])-48;
                     }
 
                 }
@@ -79,26 +243,15 @@ int main(int argc,char* argv[]){
             dist.resize(numVertices,INT_MAX);
             dist[sourceNode]=0;
 
-        // //Initialize the adj matrix
+        //Print the adj matrix
         // for(int i=0;i<numVertices;i++){
-        //     getline(cin,line);
-        //     stringstream s(line);
-
-        //     s>>tmp;
-        //     for(int j=0;j<numVertices;j++){
-        //         adjMatrix[i].push_back((int)(tmp[j])-48);//TODO: Fix this so weights can be more than one char
+        //     for(int k=0;k<numVertices;k++){
+        //         cout<<adjMatrix[i][k]<<" ";
         //     }
-
-
+        //     cout<<endl;
         // }
 
-        //Print the adj matrix
-        for(int i=0;i<numVertices;i++){
-            for(int k=0;k<numVertices;k++){
-                cout<<adjMatrix[i][k]<<" ";
-            }
-            cout<<endl;
-        }
+        parallelStarttime=MPI_Wtime();
     }
 
     auto Compare = [](Node a, Node b) { return a.dist <b.dist ;};
@@ -110,20 +263,13 @@ int main(int argc,char* argv[]){
     MPI_Bcast(&sourceNode,1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
     MPI_Bcast(&destination,1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
 
-    // cout<<"DIST SIZE IS: "<<dist.size()<<" FOR RANK: "<<mpiRank<<endl;
     if(mpiRank!=mpiRoot){
         dist.resize(numVertices,INT_MAX);//local
         dist[sourceNode]=0;
-        // cout<<"DIST SIZE IS: "<<dist.size()<<" FOR RANK: "<<mpiRank<<endl;
     }
 
-        //   cout<<"THE ELEMENTS IN DIST RANK:"<<mpiRank<<"ARE: ";
-        // for(int i=0;i<numVertices;i++){
-        //     cout<<dist[i]<<" ";
-        // }
-        // cout<<endl;
-    vector<int>globalPrev(numVertices,-1);
-    vector<int>localPrev(numVertices,-1);
+    // vector<int>globalPrev(numVertices,-1);
+    // vector<int>localPrev(numVertices,-1);
     vector<Node>myMinVec;//local
     if(mpiRank!=mpiRoot){
         adjMatrix.resize(numVertices,vector<int>(numVertices,0));//local
@@ -131,10 +277,6 @@ int main(int argc,char* argv[]){
     vector<int>explored(numVertices,0);//local
     int localMinimumPair[2];
     int globalMinimumPair[2];
-
-    //Broadcast vectors
-    // MPI_Bcast(&dist[0],numVertices,MPI_INT,mpiRoot,MPI_COMM_WORLD);
-    // MPI_Bcast(&explored,numVertices,MPI_INT,mpiRoot,MPI_COMM_WORLD);
 
     //Send each row to the other nodes
     if(mpiRank==mpiRoot){
@@ -147,28 +289,18 @@ int main(int argc,char* argv[]){
     MPI_Bcast(&globalMinimumPair[0],1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
     MPI_Bcast(&globalMinimumPair[1],1,MPI_INT,mpiRoot,MPI_COMM_WORLD);
 
-    // cout<<"ADJ MATRIX FOR RANK: "<<mpiRank<<endl;
-    // for(int i=0;i<numVertices;i++){
-    //     for(int j=0;j<numVertices;j++){
-    //         cout<<adjMatrix[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
     int myFirstVertexIndex=(mpiRank*numVertices)/mpiSize;
     int myLastVertexIndex=((mpiRank+1)*numVertices)/mpiSize-1;
 
     //Copy locally assigned vertices to local vector
     for(int i=myFirstVertexIndex;i<=myLastVertexIndex;i++){
         myMinVec.push_back(Node(i,dist[i]));
-        // cout<<"PUSHED BACK FOR RANK: "<<mpiRank<<endl;
     }
-    // cout<<"MY MINVEC SIZE IS: "<<myMinVec.size()<<" FOR RANK: "<<mpiRank<<" WITH VERTEX ID OF "<<myMinVec[0].id<<endl;
     //sort the local vector
     sort(myMinVec.begin(),myMinVec.end(),Compare);
 
     while(!explored[destination]){
-        // cout<<"RANK: "<<mpiRank<<" ENTERED"<<endl;
+
         if(mpiRank==mpiRoot){
             globalMinimumDistance=INT_MAX;
         }
@@ -177,10 +309,9 @@ int main(int argc,char* argv[]){
         localMinimumPair[1]=-1;
         for(int k=0;k<myMinVec.size();k++){
             Node v=myMinVec[k];
-            // cout<<"HELLO"<<endl;
 
             if(explored[v.id]==0){
-                // cout<<"RANK: "<<mpiRank<<" ENTERED"<<endl;
+
                 if(v.dist<globalMinimumDistance){
                     //Send to the root node
 
@@ -191,11 +322,8 @@ int main(int argc,char* argv[]){
                 }
             }
         }
-        // cout<<"GLOBAL MINIMUM VERTEX IS: "<<globalMinimumPair[1]<<" AND THE GLOBAL MINIMUM DIST IS: "<<globalMinimumPair[0]<<endl;
-        // cout<<"LOCAL MINIMUM VERTEX IS: "<<localMinimumPair[1]<<" AND THE LOCAL MINIMUM DIST IS: "<<localMinimumPair[0]<<endl;
-        MPI_Allreduce(&localMinimumPair,&globalMinimumPair,1,MPI_2INT,MPI_MINLOC,MPI_COMM_WORLD);
 
-        // cout<<"GLOBAL MINIMUM VERTEX IS: "<<globalMinimumPair[1]<<" AND THE GLOBAL MINIMUM DIST IS: "<<globalMinimumPair[0]<<endl;
+        MPI_Allreduce(&localMinimumPair,&globalMinimumPair,1,MPI_2INT,MPI_MINLOC,MPI_COMM_WORLD);
 
         if(mpiRank==mpiRoot){
             dist[globalMinimumPair[1]]=globalMinimumPair[0];
@@ -219,34 +347,44 @@ int main(int argc,char* argv[]){
                     //Sort the local vector
                     sort(myMinVec.begin(),myMinVec.end(),Compare);
                     //update prev
-                    localPrev[myMinVec[vertexIndex].id]=globalMinimumVertex;
+                    // localPrev[myMinVec[vertexIndex].id]=globalMinimumVertex;
                 }
             }
         }
     }
-    if(mpiRank!=mpiRoot){
-        MPI_Send(&localPrev[0],numVertices,MPI_INT,mpiRoot,0,MPI_COMM_WORLD);
-    }else{
-        for(int otherProcs=1;otherProcs<mpiSize;otherProcs++){
-            MPI_Recv(&localPrev[0],numVertices,MPI_INT,otherProcs,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-            for(int i=0;i<numVertices;i++){
-                if(localPrev[i]!=-1){
-                    globalPrev[i]=localPrev[i];
-                }
-            }
-        }
-    }
+    // if(mpiRank!=mpiRoot){
+    //     MPI_Send(&localPrev[0],numVertices,MPI_INT,mpiRoot,0,MPI_COMM_WORLD);
+    // }else{
+    //     for(int otherProcs=1;otherProcs<mpiSize;otherProcs++){
+    //         MPI_Recv(&localPrev[0],numVertices,MPI_INT,otherProcs,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    //         for(int i=0;i<numVertices;i++){
+    //             if(localPrev[i]!=-1){
+    //                 globalPrev[i]=localPrev[i];
+    //             }
+    //         }
+    //     }
+    // }
 
 
     if(mpiRank==mpiRoot){
-        cout<<"The shortest path from: "<<sourceNode<<" to node: "<<destination<<" is "<<globalMinimumDistance<<endl;
-        cout<<"The path to the destination to the source node is: ";
-        int currNode=destination;
-        while(currNode!=-1){
-            cout<<currNode<<" ";
-            currNode=globalPrev[currNode];
-        }
+        parallelEndtime=MPI_Wtime();
         cout<<endl;
+        // cout<<"PARALLEL: The shortest path from: "<<sourceNode<<" to node: "<<destination<<" is "<<globalMinimumDistance<<endl;
+        cout<<"PARALLEL: Took "<<parallelEndtime-parallelStarttime<<" seconds to complete"<<endl;
+
+        double serialStarttime, serialEndtime;
+        int serialShortestDist;
+
+        serialStarttime=MPI_Wtime();
+        serialShortestDist=serialDijkstra(adjMatrix,numVertices,sourceNode,destination);
+        serialEndtime=MPI_Wtime();
+
+        // cout<<"SERIAL: The shortest path from: "<<sourceNode<<" to node: "<<destination<<" is "<<serialShortestDist<<endl;
+        cout<<"SERIAL: Took "<<serialEndtime-serialStarttime<<" seconds to complete"<<endl;
+
+        cout<<"SPEEDUP: "<<(serialEndtime-serialStarttime)/(parallelEndtime-parallelStarttime)<<endl;
+
+        verify(globalMinimumDistance,serialShortestDist);
     }
 
     MPI_Finalize();
